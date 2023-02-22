bcf76b3c21a717193b29a5a6aa8bf7ded14e46fd
Ariel Campos <ariel.campos94@yahoo.com>
2023-02-22 10:01:09 -0300

    added chai into the exercise

--- tests/firstTest.js
+++ tests/firstTest.js
@@ -1,15 +1,23 @@
 const { Builder, By, Key } = require('selenium-webdriver');
-require('geckodriver');
+const should = require('chai').should();
+require('chromedriver');
 
 async function example() {
     // Launch the browser
-    let driver = await new Builder().forBrowser('firefox').build();
+    let driver = await new Builder().forBrowser('chrome').build();
 
     // Navigate to our application
     await driver.get('https://lambdatest.github.io/sample-todo-app/');
 
     //add a todo
     await driver.findElement(By.id('sampletodotext')).sendKeys('Learning Selenium', Key.RETURN);
+
+    // assert using node assertion
+    let todoText = await driver.findElement(By.xpath('//li[last()]')).getText();
+    //assert.strictEqual(todoText, 'Learning Selenium');
+
+    // assert using chai should
+    todoText.should.equal('Learning Java');
 
     // close the browser
     await driver.quit();
